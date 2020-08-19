b9f62cfcb69fe3045d24b36c63ff586d9611d5bf
bkushigian <bkushigian@gmail.com>
2020-08-19 14:57:48 -0700

    Main method writes to file using new infra

--- cornelius/src/main.rs
+++ cornelius/src/main.rs
@@ -3,8 +3,9 @@ pub mod delta;
 pub mod peg;
 pub mod rewrites;
 mod tests;
+mod util;
 
-use crate::subjects::run_on_subjects_file;
+use crate::subjects::{run_on_subjects_file, write_results_to_file};
 use std::env;
 
 #[macro_use] extern crate log;
@@ -15,7 +16,23 @@ fn main() -> Result<(), String> {
     let subject_files = &args[1..];
 
     for subj_file in subject_files {
-        let _ = run_on_subjects_file(subj_file);
+        match run_on_subjects_file(subj_file) {
+            Ok(subjects) => {
+                let mut equiv_file = String::from(subj_file);
+                equiv_file.push_str(".equiv-class");
+
+                match  write_results_to_file(&subjects, equiv_file.as_str()) {
+                    Err(e) => {
+                        println!("Error writing results of subject file {} to equiv-class file {}.",
+                            subj_file, equiv_file);
+                        println!("    {}", e);
+                    }
+                    Ok(()) => (),
+                }
+            }
+            Err(msg) =>
+                println!("Error running on subject file {}:\n    {}", subj_file, msg),
+        };
     }
     Ok(())
 }
