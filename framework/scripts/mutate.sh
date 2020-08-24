166e45c02bf28f8808f4e75a82db4e5abc386753
bkushigian <bkushigian@gmail.com>
2020-08-24 12:18:42 -0700

    Added usage to mutate.sh

--- framework/scripts/mutate.sh
+++ framework/scripts/mutate.sh
@@ -2,13 +2,47 @@
 
 source util.sh
 
-echo "major: $MAJOR_JAR"
+################################################################################
+# Print a usage screen with an optional message
+function usage {
+  printf "$(bold "usage:") ./mutate.sh path/to/File.java\n"
+  if [ ! -z "$1" ]
+  then
+    printf "    $(bold "Reason: ")$1\n\n"
+  fi
+  printf "    This command invokes the Major Javac plugin\n"
+  printf "\n"
+  printf "$(bold "Environment Variables")\n"
+  printf "%s\n" "---------------------"
+  printf "   MML: path of the compiled MML file. Default: 'mml/all.mml.bin'\n"
+  printf "   MAJOR_JAR: path to the Major Javac Plugin jar file\n"
+  exit 1
+}
+
+################################################################################
+# Parse args/sanitize input
+if [ "--help" == "$1" ]
+then
+  usage
+fi
+
+if [ -z "$1" ]
+then
+  usage "No argument provided"
+fi
 
 if [ -z "$MML" ]
 then
   MML="mml/all.mml.bin"
 fi
 
+if [ -z "$MAJOR_JAR" ]
+then
+   usage "MAJOR_JAR environment variable must be set"
+fi
+
+################################################################################
+# Business logic
 function generate_mutants {
   dir=$(realpath "$1")
   java_file="$2"
@@ -18,7 +52,6 @@ function generate_mutants {
   javac -Xplugin:"MajorPlugin mml:$MML export.mutants" -cp "$MAJOR_JAR" "$java_file"
   POPD
 }
-
 
 dir=$(dirname "$1")
 base=$(basename "$1")
