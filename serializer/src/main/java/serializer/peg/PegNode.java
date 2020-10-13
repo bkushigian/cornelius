15a7160291624ec26e3c92ebe1217c1a311eda9b
bkushigian <bkushigian@gmail.com>
2020-10-13 16:21:01 -0700

    asOpNode

--- serializer/src/main/java/serializer/peg/PegNode.java
+++ serializer/src/main/java/serializer/peg/PegNode.java
@@ -188,6 +188,11 @@ public abstract class PegNode {
         }
 
         @Override
+        public Optional<OpNode> asOpNode() {
+            return Optional.of(this);
+        }
+
+        @Override
         public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
