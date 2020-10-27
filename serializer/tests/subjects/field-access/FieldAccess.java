0d7285ff0580a2b17e4ebf8f8654db44565f6544
bkushigian <bkushigian@gmail.com>
2020-10-26 19:40:08 -0700

    Updated comments in FieldAccess.java

--- serializer/tests/subjects/field-access/FieldAccess.java
+++ serializer/tests/subjects/field-access/FieldAccess.java
@@ -4,93 +4,6 @@ public class FieldAccess {
     FieldAccess fa = this;
 
     /**
-     * This tests more field writing and reading, using overloaded names, and doing multiple rewrites.
-     *
-     * Expected PEG:
-     */
-    int test(int ex, int y) {
-        // Heap: (heap 0)
-        // Context: {ex -> (var ex), y -> (var y)}
-
-        int a = x;
-        // Heap: (heap 0)
-        // Context:
-        //     {
-        //       ex -> (var ex),
-        //       y  -> (var y),
-        //       a  -> (rd (path (var this) (derefs x)) (heap 0))
-        //     }
-
-        int b = y;
-        // Heap: (heap 0)
-        // Context:
-        //     {
-        //       ex -> (var ex),
-        //       y  -> (var y),
-        //       a  -> (rd (path (var this) (derefs x)) (heap 0)),
-        //       b  -> (var y),
-        //     }
-
-        int c = fa.fa.y;
-        // Heap: (heap 0)
-        // Context:
-        //     {
-        //       ex -> (var ex),
-        //       y  -> (var y),
-        //       a  -> (rd (path (var this) (derefs x)) (heap 0)),
-        //       b  -> (var y),
-        //       // fa: (rd (rd (path (var this) (derefs fa)))
-        //       c  -> (rd (path (rd (path (rd (path (var this) (derefs fa)) (heap 0)) (derefs fa)) (heap 0)) (derefs y)) (heap 0))
-        //     }
-
-        x = ex;
-        // Heap: (wr (path (var this) (derefs x)) (var ex) (heap 0))
-        // Context:
-        //     {
-        //       ex -> (var ex),
-        //       y  -> (var y),
-        //       a  -> (rd (path (var this) (derefs x)) (heap 0)),
-        //       b  -> (var y),
-        //       c  -> (rd (path (rd (path (rd (path (var this) (derefs fa)) (heap 0)) (derefs fa)) (heap 0)) (derefs y)) (heap 0))
-        //     }
-
-        this.y = y;
-        // Heap: (wr (path (var this) (derefs y)) (var y) (wr (path (var this) (derefs x)) (var ex) (heap 0)))
-        // Context:
-        //     {
-        //       ex -> (var ex),
-        //       y  -> (var y),
-        //       a  -> (rd (path (var this) (derefs x)) (heap 0)),
-        //       b  -> (var y),
-        //       c  -> (rd (path (rd (path (rd (path (var this) (derefs fa)) (heap 0)) (derefs fa)) (heap 0)) (derefs y)) (heap 0))
-        //     }
-        this.y = b;
-        // Heap: (wr (path (var this) (derefs y)) (var y) (wr (path (var this) (derefs y)) (var y) (wr (path (var this) (derefs x)) (var ex) (heap 0))))
-        // Context:
-        //     {
-        //       ex -> (var ex),
-        //       y  -> (var y),
-        //       a  -> (rd (path (var this) (derefs x)) (heap 0)),
-        //       b  -> (var y),
-        //       c  -> (rd (path (rd (path (rd (path (var this) (derefs fa)) (heap 0)) (derefs fa)) (heap 0)) (derefs y)) (heap 0))
-        //     }
-
-        int result = a + b + c;
-        // Heap: (wr (path (var this) (derefs y)) (var y) (wr (path (var this) (derefs y)) (var y) (wr (path (var this) (derefs x)) (var ex) (heap 0))))
-        // Context:
-        //     {
-        //       ex     -> (var ex),
-        //       y      -> (var y),
-        //       a      -> (rd (path (var this) (derefs x)) (heap 0)),
-        //       b      -> (var y),
-        //       c      -> (rd (path (rd (path (rd (path (var this) (derefs fa)) (heap 0)) (derefs fa)) (heap 0)) (derefs y)) (heap 0))
-        //       result -> (+ (+ (rd (path (var this) (derefs x)) (heap 0)) (var y)) (rd (path (rd (path (rd (path (var this) (derefs fa)) (heap 0)) (derefs fa)) (heap 0)) (derefs y)) (heap 0)))
-        //     }
-        return result;
-    }
-
-
-    /**
      * Helper method
      */
     public FieldAccess getFieldAccess() {
@@ -98,19 +11,24 @@ public class FieldAccess {
     }
 
     /**
-     * This test case checks dereferencing fields
-     *
-     * Expected PEG:
+     * simpleFieldAccess()
      * <target-peg>
-     * (let [heap  (initial-heap)
-     *       peg1  (rd (param "this") "fa" heap)
-     *       heap  (update-exception-status heap (is-null? (param "this")) (exception "java.lang.NullPointerException"))
-     *       peg2  (rd peg1 "fa" heap)
-     *       heap  (update-exception-status heap (is-null? peg1) (exception "java.lang.NullPointerException"))
-     *       peg3  (rd peg2 "y"  heap)
-     *       heap  (update-exception-status heap (is-null? peg2) (exception "java.lang.NullPointerException"))]
-     *   (method-root peg3 heap))
-     *   
+     * (let
+     *  [heap
+     *   (initial-heap)
+     *   peg1
+     *   (rd (param "this") "fa" heap)
+     *   heap
+     *   (update-status-npe heap (param "this"))
+     *   peg2
+     *   (rd peg1 "fa" heap)
+     *   heap
+     *   (update-status-npe heap peg1)
+     *   peg3
+     *   (rd peg2 "y" heap)
+     *   heap
+     *   (update-status-npe heap peg2)]
+     *  (method-root peg3 heap))
      * </target-peg>
      */
     int simpleFieldAccess() {
@@ -118,12 +36,18 @@ public class FieldAccess {
     }
 
     /**
+     * methodInvocation()
      * <target-peg>
-     * (let [heap (initial-heap)
-     *       recv (param  "this")
-     *       args (actuals)
-     *       invk (invoke heap recv "getFieldAccess" args)]
-     *   (method-root (invoke->peg invk) (invoke->heap invk)))
+     * (let
+     *  [heap
+     *   (initial-heap)
+     *   recv
+     *   (param "this")
+     *   args
+     *   (actuals)
+     *   invk
+     *   (invoke heap recv "getFieldAccess" args)]
+     *  (method-root (invoke->peg invk) (invoke->heap invk)))
      * </target-peg>
      */
     FieldAccess methodInvocation() {
@@ -149,6 +73,7 @@ public class FieldAccess {
         return fa.getFieldAccess();
     }
 
+
     /**
      * methodInvocation3()
      * <target-peg>
@@ -168,14 +93,10 @@ public class FieldAccess {
      *   peg2
      *   (rd peg1 "fa" heap)
      *   heap
-     *   (update-exception-status
-     *    heap
-     *    (is-null? peg1)
-     *    (exception "java.lang.NullPointerException"))]
+     *   (update-status-npe heap peg1)]
      *  (method-root peg2 heap))
      * </target-peg>
      */
-
     FieldAccess methodInvocation3() {
         return fa.getFieldAccess().fa;
     }
@@ -199,17 +120,11 @@ public class FieldAccess {
      *   peg2
      *   (rd peg1 "fa" heap)
      *   heap
-     *   (update-exception-status
-     *    heap
-     *    (is-null? peg1)
-     *    (exception "java.lang.NullPointerException"))
+     *   (update-status-npe heap peg1)
      *   peg
      *   (rd peg2 "y" heap)
      *   heap
-     *   (update-exception-status
-     *    heap
-     *    (is-null? peg2)
-     *    (exception "java.lang.NullPointerException"))]
+     *   (update-status-npe heap peg2)]
      *  (method-root peg heap))
      * </target-peg>
      */
@@ -218,65 +133,171 @@ public class FieldAccess {
     }
 
     /**
-     * This test case tests for guards against NPEs
-     * Expected PEG:
-     * <-target-peg>
-     * (method-root (phi (exit-condition (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))) (phi (exit-condition (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))) unit (rd (path (rd (path (var this) (derefs fa)) (heap 0 unit)) (derefs x)) (heap 0 unit))) (+ (phi (exit-condition (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))) unit (rd (path (rd (path (var this) (derefs fa)) (heap 0 unit)) (derefs x)) (heap 0 unit))) 1)) (heap 0 (phi (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit))) java.lang.NullPointerException unit)))
-     * </-target-peg>
+     * possibleNPEFollowedByContextUpdate()
+     * <target-peg>
+     * (let
+     *  [heap
+     *   (initial-heap)
+     *   ctx
+     *   (new-ctx-from-params)
+     *   peg
+     *   (rd (param "this") "fa" heap)
+     *   peg-tmp
+     *   (rd peg "x" heap)
+     *   exitc
+     *   (is-null? peg)
+     *   heap
+     *   (update-status-npe heap peg)
+     *   ctx
+     *   (add-exit-condition-to-ctx ctx exitc)
+     *   peg
+     *   peg-tmp
+     *   ctx
+     *   (update-key-in-ctx ctx "x" peg)
+     *   x
+     *   (lookup-in-ctx ctx "x")
+     *   peg
+     *   (opnode "+" x (int-lit 1))
+     *   ctx
+     *   (update-key-in-ctx ctx "x" peg)
+     *   peg
+     *   (lookup-in-ctx ctx "x")]
+     *  (method-root peg heap))
+     * </target-peg>
      */
     int possibleNPEFollowedByContextUpdate() {
-        // Heap:      (heap 0 unit)
-        // Context:   {}
-        // ExitConds: []
         int x = fa.x;
-        // this.fa:
-        //     Heap: (heap 0 unit)
-        //     ExitConds: []
-        //     PEG1: (rd (path (var this) (derefs fa)) (heap 0 unit))
-        //     Context: {}
-        //
-        // this.fa.x:
-        //     Heap: (heap 0 (phi (exit-condition (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))) java.lang.NullPointerException unit))
-        //     ExitConds: [(isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))]
-        //     PEG2: (rd (path (rd (path (var this) (derefs fa)) (heap 0 unit)) (derefs x)) (heap 0 unit))
-        //     Context: {}
-        //    
-        //     NOTES: The exception status is now a PHI node that evaluates to
-        //        `java.lang.NullPointerException` whenever this.x is `null`; It
-        //        is `unit` otherwise.
-        // assignment:
-        //     Heap: (heap 0 (phi (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit))) java.lang.NullPointerException unit))
-        //     ExitConds: [(isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))]
-        //     PEG2: (rd (path (rd (path (var this) (derefs fa)) (heap 0 unit)) (derefs x)) (heap 0 unit))
-        //     Context: {x -> (phi (exit-condition (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))) unit (rd (path (rd (path (var this) (derefs fa)) (heap 0 unit)) (derefs x)) (heap 0 unit)))}
-        //
-        //     NOTES: The assignment updates the context, but only when all exit
-        //         conditions are false. The heap is the same, as are the exit
-        //         conditions. The context now maps x to a PHI node that branches
-        //         over the exit condition.
-        //
-        //         The THEN branch contains unit, since this means that a NPE
-        //         occurred before `x` could be assigned a value (note, x
-        //         already was assigned to a value, this value would replace
-        //         `unit`).
-        //
-        //         The ELSE branch contains a `rd` node corresponding to
-        //         derefrencing (this.fa).x. This represents a successful read.
-
         x = x + 1;
-        // x = x + 1:
-        //
-        //     Heap: (heap 0 (phi (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit))) java.lang.NullPointerException unit))
-        //     Context: {x -> (phi (exit-condition (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))) (phi (exit-condition (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))) unit (rd (path (rd (path (var this) (derefs fa)) (heap 0 unit)) (derefs x)) (heap 0 unit))) (+ (phi (exit-condition (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))) unit (rd (path (rd (path (var this) (derefs fa)) (heap 0 unit)) (derefs x)) (heap 0 unit))) 1))}
-        //
-        //     NOTES: We are updating x again, and this assignment needs to be
-        //         wrapped in a guard against a possible NPE. This is actually a
-        //         little unsatisfying since we've already checked for a NPE,
-        //         and this is a possible future improvement
-        //
-        //         The heap and exit conditions are not updated at all
         return x;
-        // The resulting method-root is:
-        // (method-root (phi (exit-condition (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))) (phi (exit-condition (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))) unit (rd (path (rd (path (var this) (derefs fa)) (heap 0 unit)) (derefs x)) (heap 0 unit))) (+ (phi (exit-condition (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit)))) unit (rd (path (rd (path (var this) (derefs fa)) (heap 0 unit)) (derefs x)) (heap 0 unit))) 1)) (heap 0 (phi (isnull? (rd (path (var this) (derefs fa)) (heap 0 unit))) java.lang.NullPointerException unit)))
+    }
+
+    /**
+     * testFieldAccess1(int,int)
+     * <target-peg>
+     * (let
+     *  [heap
+     *   (initial-heap)
+     *   ctx
+     *   (new-ctx-from-params "ex" "y")
+     *   peg
+     *   (rd (param "this") "x" heap)
+     *   ctx
+     *   (update-key-in-ctx ctx "a" peg)
+     *   y
+     *   (lookup-in-ctx ctx "y")
+     *   ctx
+     *   (update-key-in-ctx ctx "b" y)
+     *   fa
+     *   (rd (param "this") "fa" heap)
+     *   fa-fa
+     *   (rd fa "fa" heap)
+     *   heap
+     *   (update-status-npe heap fa)
+     *   ctx
+     *   (add-exit-condition-to-ctx ctx (is-null? fa))
+     *   fa-fa-y
+     *   (rd fa-fa "y" heap)
+     *   heap
+     *   (update-status-npe heap fa-fa)
+     *   ctx
+     *   (add-exit-condition-to-ctx ctx (is-null? fa-fa))
+     *   ctx
+     *   (update-key-in-ctx ctx "c" fa-fa-y)
+     *   a+b
+     *   (opnode "+" (lookup-in-ctx ctx "a") (lookup-in-ctx ctx "b"))
+     *   a+b+c
+     *   (opnode "+" a+b (lookup-in-ctx ctx "c"))
+     *   x
+     *   (rd (param "this") "x" heap)
+     *   a+b+c+x
+     *   (opnode "+" a+b+c x)
+     *   ctx
+     *   (update-key-in-ctx ctx "result" a+b+c+x)
+     *   result
+     *   (lookup-in-ctx ctx "result")]
+     *  (method-root result heap))
+     * </target-peg>
+     */
+    int testFieldAccess1(int ex, int y) {
+        int a = x;
+        int b = y;
+        int c = fa.fa.y;
+
+        int result = a + b + c + x;
+        return result;
+    }
+
+    /**
+     * testFieldAccess2(int,int)
+     * <target-peg>
+     * (let
+     *  [heap
+     *   (initial-heap)
+     *   ctx
+     *   (new-ctx-from-params "ex" "y")
+     *   peg
+     *   (rd (param "this") "x" heap)
+     *   ctx
+     *   (update-key-in-ctx ctx "a" peg)
+     *   y
+     *   (lookup-in-ctx ctx "y")
+     *   ctx
+     *   (update-key-in-ctx ctx "b" y)
+     *   fa
+     *   (rd (param "this") "fa" heap)
+     *   fa-fa
+     *   (rd fa "fa" heap)
+     *   heap
+     *   (update-status-npe heap fa)
+     *   ctx
+     *   (add-exit-condition-to-ctx ctx (is-null? fa))
+     *   fa-fa-y
+     *   (rd fa-fa "y" heap)
+     *   heap
+     *   (update-status-npe heap fa-fa)
+     *   ctx
+     *   (add-exit-condition-to-ctx ctx (is-null? fa-fa))
+     *   ctx
+     *   (update-key-in-ctx ctx "c" fa-fa-y)
+     *   ex
+     *   (lookup-in-ctx ctx "ex")
+     *   y
+     *   (lookup-in-ctx ctx "y")
+     *   cond
+     *   (opnode "<" ex y)
+     *   ctx-thn
+     *   (update-key-in-ctx ctx "a" y)
+     *   ctx-els
+     *   (update-key-in-ctx ctx "a" ex)
+     *   ctx
+     *   (ctx-join cond ctx-thn ctx-els)
+     *   a+b
+     *   (opnode "+" (lookup-in-ctx ctx "a") (lookup-in-ctx ctx "b"))
+     *   a+b+c
+     *   (opnode "+" a+b (lookup-in-ctx ctx "c"))
+     *   x
+     *   (rd (param "this") "x" heap)
+     *   a+b+c+x
+     *   (opnode "+" a+b+c x)
+     *   ctx
+     *   (update-key-in-ctx ctx "result" a+b+c+x)
+     *   result
+     *   (lookup-in-ctx ctx "result")]
+     *  (method-root result heap))
+     * </target-peg>
+     */
+    int testFieldAccess2(int ex, int y) {
+        int a = x;
+        int b = y;
+        int c = fa.fa.y;
+
+        if (ex < y) {
+            a = y;
+        } else {
+            a = ex;
+        }
+
+        int result = a + b + c + x;
+        return result;
     }
 }
