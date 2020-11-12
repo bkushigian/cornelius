bb831d2e255715480039f0e0284b24a3269f5386
bkushigian <bkushigian@gmail.com>
2020-11-11 20:46:04 -0800

    Updated core

--- serializer/pegnodes/src/pegnodes/core.clj
+++ serializer/pegnodes/src/pegnodes/core.clj
@@ -1,9 +1,12 @@
 (ns pegnodes.core
   (:require  [pegnodes.pegs :refer :all])
   (:require  [pegnodes.tests.tests :refer :all])
-  (:require  [pegnodes.tests.field-access :as field-access]))
+  (:require  [pegnodes.tests.field-access])
+  (:require  [pegnodes.tests.statements])
+  )
 
 (defn -main
   "Run tests"
   []
-  (test-java-class field-access/file-path))
+  ;; (test-file pegnodes.tests.field-access/file-path)
+  (test-file pegnodes.tests.statements/file-path))
