1735c91cf0435e42becc60e7432c2965792c90a6
bkushigian <bkushigian@gmail.com>
2020-11-18 15:36:34 -0800

    --scrape-comments

--- serializer/src/main/java/serializer/peg/PegSerializer.java
+++ serializer/src/main/java/serializer/peg/PegSerializer.java
@@ -20,16 +20,27 @@ public class PegSerializer {
    */
   public static void main(String[] args) {
     final PegSerializer s = new PegSerializer(".");
-    for (String javaFile : args) {
+    boolean scrapeComments = false;
+    List<String> files = new ArrayList<>();
+    for (String arg : args) {
+      if ("--scrape-comments".equals(arg)) {
+        scrapeComments = true;
+      }
+      else {
+        files.add(arg);
+      }
+    }
+
+    for (String file : files) {
       try {
-        final Map<String, PegNode> translated = s.serialize(javaFile);
-        System.out.println("* " + javaFile);
-        for (final String meth: translated.keySet()) {
+        final Map<String, PegNode> translated = s.serialize(file, scrapeComments);
+        System.out.println("* " + file);
+        for (final String meth : translated.keySet()) {
           final PegNode node = translated.get(meth);
           System.out.printf("%s: %s\n", meth, node.toDerefString());
         }
       } catch (FileNotFoundException e) {
-        System.out.println("Couldn't load file " + javaFile);
+        System.out.println("Couldn't load file " + file);
       }
     }
   }
@@ -37,12 +48,23 @@ public class PegSerializer {
   /**
    * Serialize a Java file
    * @param javaFile the Java source file to translate
-   * @return a mapping from method names to
+   * @return a mapping from method names to resulting pegs
    * @throws FileNotFoundException if
    */
   public Map<String, PegNode> serialize(final String javaFile) throws FileNotFoundException {
+    return serialize(javaFile, false);
+  }
+
+  /**
+   * Serialize a Java file
+   * @param javaFile the Java source file to translate
+   * @param scrapeComments should comments be scraped for test pair generation?
+   * @return a mapping from method names to resulting pegs
+   * @throws FileNotFoundException if
+   */
+  public Map<String, PegNode> serialize(final String javaFile, boolean scrapeComments) throws FileNotFoundException {
     final CompilationUnit cu = StaticJavaParser.parse(new File(javaFile));
-    final SimpleJavaToPegTranslator t = new SimpleJavaToPegTranslator();
+    final SimpleJavaToPegTranslator t = new SimpleJavaToPegTranslator(scrapeComments);
     return t.translate(cu);
   }
 
