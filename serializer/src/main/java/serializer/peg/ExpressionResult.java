d8042a91f4b79176dc65db559f88ece3da3d3fcd
bkushigian <bkushigian@gmail.com>
2021-03-09 11:11:32 -0800

    Added docs to ExpressionResult

--- serializer/src/main/java/serializer/peg/ExpressionResult.java
+++ serializer/src/main/java/serializer/peg/ExpressionResult.java
@@ -1,5 +1,23 @@
 package serializer.peg;
 
+/**
+ * <p>An {@code ExpressionResult} tracks the result of visiting an expression. There are two
+ * things we need to track: (1) the resulting PEG, and (2) the changes in state reflecting
+ * the side effects of evaluating this expression.</p>
+ * <p>For instance, if the expression is {@code x + foo()}, the peg would be of the form</p>
+ *
+ * <p><code>
+ * (+ (param x)
+ *    (invoke->peg
+ *      (invoke (heap 0 unit)
+ *        (param "this")
+ *        "foo"
+ *        (actuals))))
+ * </code></p>
+ *
+ * <p>and the context would have to track the possibility that {@code foo()} threw an
+ * exception, which would be done in the context.</p>
+ */
 public class ExpressionResult {
   public final PegNode peg;
   public final PegContext context;
@@ -8,14 +26,29 @@ public class ExpressionResult {
     this.context = context;
   }
 
+  /**
+   * Return this {@code ExpressionResult} with {@code this.context.heap} replaced by {@code heap}
+   * @param heap
+   * @return
+   */
   public ExpressionResult withHeap(PegNode.Heap heap) {
     return new ExpressionResult(peg, context.withHeap(heap));
   }
 
+  /**
+   * Return this {@code ExpressionResult} with {@code this.context} replaced by {@code context}
+   * @param context
+   * @return
+   */
   public ExpressionResult withContext(PegContext context) {
     return new ExpressionResult(peg, context);
   }
 
+  /**
+   * Return this {@code ExpressionResult} with the {@code this.peg} replaced by {@code peg}
+   * @param peg
+   * @return
+   */
   public ExpressionResult withPeg(PegNode peg) {
     return new ExpressionResult(peg, context);
   }
