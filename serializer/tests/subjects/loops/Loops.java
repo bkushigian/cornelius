0ff75e8ec32155089a721d952df47a1990832827
Ryan Featherman <feathr@cs.washington.edu>
2021-06-01 14:51:51 -0700

    added more loop tests

--- serializer/tests/subjects/loops/Loops.java
+++ serializer/tests/subjects/loops/Loops.java
@@ -1,6 +1,10 @@
 public class Loops {
 
-    int program01() {
+    int isEven(int x) {
+        return x % 2 == 0;
+    }
+
+    int do_nothing_false() {
         /**
          * <cond>
          *  [blank1     (blank-node)
@@ -35,7 +39,7 @@ public class Loops {
         return 0;
     }
 
-    int program02() {
+    int false_heap_update() {
         /**
          * <expected>
          *  [ctx     (ctx-update ctx "x" (int-lit 0))]
@@ -100,7 +104,7 @@ public class Loops {
         return x;
     }
 
-    int program03() {
+    int add_in_body() {
         /**
          * <expected>
          *   [ctx     (ctx-update ctx "x" (int-lit 0))]
@@ -157,7 +161,7 @@ public class Loops {
         return x;
     }
 
-    int program04() {
+    int cond_side_effect() {
         /**
          * <expected>
          *   [ctx     (ctx-update ctx "x" (int-lit 0))]
@@ -213,5 +217,101 @@ public class Loops {
          * </expected>
          */
         return x;
+    }
+
+    int multiple_vars() {
+        /**
+         * <expected>
+         *   [ctx     (ctx-update ctx "i" (int-lit 0))
+         *    ctx     (ctx-update ctx "j" (int-lit 1))]
+         * </expected>
+         */
+        int i = 0;
+        int j = 1;
+
+        /**
+         * <cond>
+         *  [blank1         (blank-node)
+         *   blank2         (blank-node)
+         *   blank3         (blank-node)
+         *   blank4         (blank-node)
+         *   theta-i        (theta-node (ctx-lookup ctx "i") blank1)
+         *   theta-j        (theta-node (ctx-lookup ctx "j") blank2)
+         *   theta-state    (theta-node (heap->state heap) blank3)
+         *   theta-status   (theta-node (heap->status heap) blank4)
+         *   heap           (make-heap theta-state theta-status)
+         *   condition      (opnode "<" cond-i (int-lit 10))
+         *   (snapshot {:peg condition :ctx ctx :heap heap})]
+         * </cond>
+         * <body>
+         *  [i              (ctx-lookup ctx "i")
+         *   j              (ctx-lookup ctx "j")
+         *   state          (heap->state heap)
+         *   status         (heap->status heap)
+         *   update         (assign-blank blank1 i)
+         *   update         (assign-blank blank2 j)
+         *   update         (assign-blank blank3 state)
+         *   update         (assign-blank blank4 status)
+         *   (snapshot {:ctx ctx :heap heap})]
+         * </body> 
+         * <expected>      
+         *  [pass       (pass-node condition)
+         *   ctx        (ctx-update ctx "i" (eval-node theta-i pass))
+         *   ctx        (ctx-update ctx "j" (eval-node theta-j pass))
+         *   heap       (make-heap (eval-node theta-state pass) (eval-node theta-status pass))
+         *   (snapshot {:ctx ctx :heap heap})]
+         * </expected>
+         */
+        while (i < 10) {
+            /**
+             * <expected>
+             *   [i       (ctx-lookup ctx "i")
+             *    ctx     (ctx-update ctx "temp" i)]
+             * </expected>
+             */
+            int temp = i;
+
+            /**
+             * <expected>
+             *   [j       (ctx-lookup ctx "j")
+             *    ctx     (opnode "+" j j)]
+             * </expected>
+             */
+            i = j + j;
+
+            /**
+             * <expected>
+             *   [temp    (ctx-lookup ctx "temp")
+             *    ctx     (opnode "+" temp temp)]
+             * </expected>
+             */
+            j = temp + temp;
+        }
+
+        /**
+         * <expected>
+         *  [j  (ctx-lookup ctx "j") 
+         *   (snapshot {:return j})]
+         * </expected>
+         */
+        return j;
+    }
+
+    int method_call(int x) {
+        int i = 0;
+        while(isTrue(i + x)) {
+            i++;
+        }
+    }
+
+    int nested_theta() {
+        int i = 0;
+        while (i < 5) {
+            int j = 0;
+            while (i + j < 5) {
+	            j++;
+            }
+            i++;
+        }
     }
 }
