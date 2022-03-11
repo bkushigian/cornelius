696a09d046b5f635b706d8920c47aa5a7d7d5946
bkushigian <bkushigian@gmail.com>
2022-03-10 23:34:59 -0800

    Fix error message

--- serializer/src/main/java/serializer/ExprSerializer.java
+++ serializer/src/main/java/serializer/ExprSerializer.java
@@ -506,7 +506,7 @@ public class ExprSerializer {
             Expression e = StaticJavaParser.parseExpression(constMap.get(key));
             constMap2.put(key, e);
           } catch (ParseProblemException e) {
-            System.err.printf("Couldn't parse expression %s\n", e.toString());
+            System.err.printf("Couldn't parse expression %s\n", constMap.get(key));
           }
         }
         ExpressionResult expressionResult;
